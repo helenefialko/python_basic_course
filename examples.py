dd796dfb849d6cb00a7e673c8ee9d192ddaa86cf
helenefialko <legna_krad@mail.ru>
2018-03-22 21:21:49 +0200

    test

---
+++ examples.py
@@ -0,0 +1,40 @@
+name = 'fff'
+print(f'Your name is {name}')
+print('Your name is {0}'.format(name))
+print('Your name is %s' % (name))
+
+
+a=5
+print(type(a))
+
+b=5.5
+print(type(b))
+
+c={'1','2'}
+print(type(c))
+
+d='ff'
+print(type(d))
+
+e='1'
+print(type(e))
+
+f=(a,b,c)
+print(type(f))
+
+f=[a,b,c]
+print(type(f))
+
+t={"1":"hh"}
+print(type(t))
+
+
+name='Tur'
+phone_number='123'
+group='first'
+print(f'{name}:{phone_number} [{group}]')
+
+#the lesson is over
+"""gh
+jnfnr
+jrgjrgjr"""
